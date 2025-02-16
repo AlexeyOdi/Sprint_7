63ff64ef2f5f1605db954d7a22e50207794b0254
Alexey <odintsov.a.irk@gmail.com>
2025-02-16 22:22:50 +0300

    fourth revision

--- tests/test_login_courier.py
+++ tests/test_login_courier.py
@@ -1,55 +1,31 @@
-import requests
 import allure
-import helpers
-import test_urls
 import test_data
+import pytest
 
 class TestLoginCourier:
 
     @allure.title('Проверяем вход в аккаунт курьера с правильно заполненными полями')
+    @pytest.mark.usefixtures('delete_courier')
     def test_login_courier_with_necessary_fields(self):
-        courier_data = helpers.generate_courier()
-        requests.post(test_urls.main_url+test_urls.create_courier_url, data=courier_data)
-        response = requests.post(test_urls.main_url+test_urls.login_courier_url, data=courier_data)
-
-        assert response.status_code == 200 and "id" in response.text
-        requests.delete(test_urls.main_url+test_urls.create_courier_url+'/'+response.text[6:11])
+        assert self.response.status_code == 200 and "id" in self.response.text
 
     @allure.title('Проверяем вход в аккаунт курьера с несуществующим логином')
-    def test_login_courier_with_wrong_login(self):
-        courier_data = helpers.generate_courier()
-        requests.post(test_urls.main_url+test_urls.create_courier_url, data=courier_data)
-        courier_data['login'] = 'Alexey'
-        response = requests.post(test_urls.main_url+test_urls.login_courier_url, data=courier_data)
+    @pytest.mark.f_data('login')
+    def test_login_courier_with_wrong_login(self, delete_courier):
+        assert self.response.status_code == 404 and test_data.login_message_unsuccess in self.response.text
 
-        assert response.status_code == 404 and test_data.login_message_unsuccess in response.text
-        requests.delete(test_urls.main_url+test_urls.create_courier_url+'/'+response.text[6:11])
 
     @allure.title('Проверяем вход в аккаунт курьера с несуществующим паролем')
-    def test_login_courier_with_wrong_password(self):
-        courier_data = helpers.generate_courier()
-        requests.post(test_urls.main_url+test_urls.create_courier_url, data=courier_data)
-        courier_data['password'] = 'Alexey'
-        response = requests.post(test_urls.main_url+test_urls.login_courier_url, data=courier_data)
-
-        assert response.status_code == 404 and test_data.login_message_unsuccess in response.text
-        requests.delete(test_urls.main_url+test_urls.create_courier_url+'/'+response.text[6:11])
+    @pytest.mark.f_data('password')
+    def test_login_courier_with_wrong_password(self, delete_courier):
+        assert self.response.status_code == 404 and test_data.login_message_unsuccess in self.response.text
 
     @allure.title('Проверяем вход в аккаунт курьера без логина')
-    def test_login_courier_without_login(self):
-        courier_data = helpers.generate_courier()
-        requests.post(test_urls.main_url+test_urls.create_courier_url, data=courier_data)
-        del courier_data['login']
-        response = requests.post(test_urls.main_url+test_urls.login_courier_url, data=courier_data)
+    @pytest.mark.f_data('delete')
+    def test_login_courier_without_login(self, delete_courier):
+        assert self.response.status_code == 400 and test_data.login_message_without_field in self.response.text
 
-        assert response.status_code == 400 and test_data.login_message_without_field in response.text
-        requests.delete(test_urls.main_url+test_urls.create_courier_url+'/'+response.text[6:11])
-
+    @pytest.mark.usefixtures("delete_courier")
     @allure.title('Проверяем, что при входе в аккаунт курьера в теле ответа приходит id')
     def test_id_in_response(self):
-        courier_data = helpers.generate_courier()
-        requests.post(test_urls.main_url+test_urls.create_courier_url, data=courier_data)
-        response = requests.post(test_urls.main_url+test_urls.login_courier_url, data=courier_data)
-
-        assert 'id' in response.text
-        requests.delete(test_urls.main_url+test_urls.create_courier_url+'/'+response.text[6:11])
+        assert 'id' in self.response.text
